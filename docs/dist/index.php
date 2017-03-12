221a0b8837dcd06b5bf9f460c71d8d58c767f591
Rafael Munoz <freefastfire@gmail.com>
2017-03-12 13:51:07 -0700

    Added Social Media Functionality
    Updated March 12th

---
+++ docs/dist/index.php
@@ -0,0 +1,156 @@
+<?php $show_modal = false; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  </head>
+  <body>
+  <nav class="navbar navbar-toggleable-md navbar-light navbar-inverse bg-blue" id="top">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand mb-0" href="index.php">Repfinder</a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#search">Search</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">Welcome to Repfinder.</h1>
+    <p class="lead">Have you ever sent a letter to your senator? What about your state legislators or even the mayor of your town? Their contact information is scattered throughout the web on various official and unofficial websites. Our website helps you find all the government representatives for an address. Stay informed.</p>
+  </div>
+</div>
+
+<section id="search">
+  <div class="container-fluid bg-blue bigger-box">
+    <div class="row">
+      <div class="col-lg-4 offset-lg-2 text-center">
+        <h1 class="display-4">Enter your Address: </h1>
+      </div>
+      <div class="col-lg-4 text-center align-middle">
+
+        <form action="" class="form-signin" method="post">
+            <!-- <div class="group">
+              <input type="text" id="searchaddress" name="searchaddress" value="<?php echo $searchaddress; ?>">
+              <button class="btn btn-primary">Search</button>   
+              <span class="highlight"></span>
+              <span class="bar"></span> -->
+            <input type="hidden" value="checked" name="firsttime">
+            <input type="text" id="searchaddress" name="searchaddress">
+            <button class="btn btn-primary">Search</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+  if(isset($_POST['searchaddress'])){
+    $searchaddress = $_POST['searchaddress']; 
+    $searchaddress_final = str_replace(' ', '%20', $searchaddress);
+  }
+  else {
+    //$searchaddress = '18111 Nordhoff St. Northridge CA'; 
+    //$searchaddress_final = '18111%20Nordhoff%20St.%20Northridge%20CA'; 
+  }
+  $data = json_decode(file_get_contents('https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyA2oUu1o-zH4MvJkIrxZ1-imss5gH_P_cI&address='.$searchaddress_final));  
+
+  if (isset($_POST['firsttime'])){
+    $check = count($data);
+    if ($check == 0) {
+      $show_modal = true;
+    }  
+  }
+
+  $jobs = [];
+  // loop through each office 
+  foreach ($data->offices as $office) {
+    // loop through each officialIndices array
+    if (isset($office->officialIndices)) {
+      foreach ($office->officialIndices as $officialIndices) {
+        $jobs += [ $officialIndices => $office->name];
+      }
+    }
+  }
+  $i = 0;
+  echo '<div class="container-fluid">';
+  $list = count($jobs);//list counts how many results came back from Google
+    if($list>0){
+      echo '<br><h2 class="text-center display-3">This is who represents you:</h2><br>';
+      echo '<div class="container">';
+      echo '<div class="card-columns ">';
+    } else {
+      echo '<div class="container">';
+      echo '<div class="card-columns">';
+    }
+  foreach ($data->officials as $person) {
+    //$searchaddress_final = str_replace(' ', '%20', $searchaddress);
+    echo '<div class="card text-center reveal-1">';  
+    echo '<div class="card-header">'.(isset($person->party)? $person->party :'Not Listed').'</div>';
+    echo '<img src="'.(isset($person->photoUrl)? $person->photoUrl : 'http://placehold.it/360x250?text=Not+Available').'" class="img-fluid border-0 mx-auto card-img-top">';
+    echo '<div class="card-block">';
+    echo '<h4 class="card-title">'.$person->name.'</h4>';
+    echo '<h6 class="card-subtitle mb-2 text-muted">'.$jobs[$i].'</h6></div>';
+    //echo '<p class="card-text"> City: '.$person->address[0]->city.'</p>';
+    echo '<ul class="list-group list-group-flush">';
+    echo '<li class="list-group-item">City: '.(isset($person->address[0]->city)? $person->address[0]->city :'Not Listed').'</li>';
+    echo '<li class="list-group-item">Phone: '.(isset($person->phones[0])? $person->phones[0] :'Not Listed').'</li>';
+    echo ''.(isset($person->emails[0])? '<li class="list-group-item">Email: '.$person->emails[0].'</li>' :'').'';
+    echo '</ul>';
+    echo '<div class="card-block">';
+    echo '<a href="'.$person->urls[0].'" class="card-link">Website</a></div>';
+    echo '</div>';
+    $i++;
+  }
+?>
+    </div> <!--close card-columns-->
+  </div> <!--close container-->
+</div> <!--close color-container-->
+
+
+<footer class="bd-footer text-muted text-center">
+  <div class="container">
+      <p>Made by Rafa &hearts; Built with the <a href="https://developers.google.com/civic-information/" target="_blank">Google Civic Information API</a>.
+      </p>
+  </div> <!--close container-->
+</footer> <!--close footer-->
+
+<?php if($show_modal):?>
+<script>$(document).ready(function(){$("#pModal").modal("show");});</script>
+<?php endif;?>
+
+<div class="modal fade" id="pModal" tabindex="-1" role="dialog" aria-labelledby="pModalLabel">
+  <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header alert-warning">
+          <h4 class="modal-title" id="pModalLabel">Error: Wrong Address</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+      </div>
+  </div>
+</div>
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="../bower_components/scrollreveal/dist/scrollreveal.js"></script>
+    <script src="js/smooth.js"></script>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.js"></script>
+  </body>
+</html>
